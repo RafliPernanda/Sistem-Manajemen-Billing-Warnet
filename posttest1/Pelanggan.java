289c2e55650a5afa1c25eb96e298499cd48b03ad
RafliPernanda <raflipernanda212@gmail.com>
2026-03-15 12:39:57 +0800

    up pt 2

---
+++ posttest1/Pelanggan.java
@@ -0,0 +1,9 @@
+public class Pelanggan {
+    public String id;
+    public String nama;
+
+    public Pelanggan(String id, String nama) {
+        this.id = id;
+        this.nama = nama;
+    }
+}
